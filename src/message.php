7ca1d95e9a1e3238e89f37066427c5f244f36222
leomatt547 <leo.matt.547@gmail.com>
2021-04-14 22:00:54 +0700

    Spek 2a,b

--- src/message.php
+++ src/message.php
@@ -1,4 +1,3 @@
-<!-- Created By CodingNepal -->
 <?php
 // connecting to database
 $conn = mysqli_connect("sql6.freesqldatabase.com", "sql6405141", "BkxHy17U62","sql6405141") or die("Database Error");
@@ -16,61 +15,56 @@ $ArrKataPenting = "";
 $Topik = "NULL";
 $ArrTopik = "NULL";
 
+function date_sort($a, $b) {
+    return strtotime($a) - strtotime($b);
+}
+
+
 if (preg_match("/HELP|help|Help|[Bb]agaimana/",$getMesg)){
     echo "Tambah Deadline: Masukkan tanggal, matkul, jenis, topik(opsional) <br> Apa? : Menampilkan Deadline";
 }
 
-if (preg_match("/[Rr]eset|[Hh]apus [Ss]emua|[Dd]elete [Aa]ll/",$getMesg)){
-    $sql = "ALTER TABLE tabel DROP id;";
-    $sql .= "ALTER TABLE  `tabel` ADD `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST";
-    if (mysqli_multi_query($conn, $sql)) {
-        echo "Deadline berhasil disetting ulang";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-}
-if (preg_match_all("/(0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-[0-9]{4}/",$getMesg,$arr_date)) {
-    for ($i = 0; $i < count($arr_date[0]); $i++){
-        $arr_ymd = DateTime::createFromFormat('d-m-Y', $arr_date[0][$i])->format('Y-m-d');
-        $date = sprintf($arr_ymd);
-        echo $date."\n";
-    }
-    if (preg_match_all("/[A-Z]{2}[0-9]{4}/",$getMesg,$ArrMatkul)){
-        $Matkul = sprintf($ArrMatkul[0][0]);
-        echo $Matkul."\n";
-        if (preg_match_all("/Tubes|Tucil|Kuis|Praktikum|UTS|UAS/",$getMesg,$ArrKataPenting)){
-            $KataPenting = sprintf($ArrKataPenting[0][0]);
-            echo $KataPenting."\n";
-            if(preg_match_all("/topik(.*)/", $getMesg,$ArrTopik)){
-                $Topik = sprintf($ArrTopik[1][0]);
-                echo $Topik."\n";
-            }else{
-                $Topik = "NULL";
-            }
 
-            $sql = "INSERT INTO tabel (`date`, `matkul`, `katapenting`, `topik`) VALUES ('$date', '$Matkul', '$KataPenting','$Topik')";
-
-            if (mysqli_query($conn, $sql)) {
-                echo "<br> Deadline berhasil dimasukkan";
-            } else {
-                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-            }
-
+if (preg_match("/[Aa]pa/",$getMesg) && preg_match("/[Dd]eadline/",$getMesg)){
+    if(preg_match_all("/(0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-[0-9]{4}/",$getMesg,$arr_date)){
+        //sort dulu tanggalnya supaya between di sql ga salah
+        //Deadline Rentang waktu tertentu
+        $arr_ymd = DateTime::createFromFormat('d-m-Y', $arr_date[0][0])->format('Y-m-d');
+        $date1 = sprintf($arr_ymd);
+        $arr_ymd2 = DateTime::createFromFormat('d-m-Y', $arr_date[0][1])->format('Y-m-d');
+        $date2 = sprintf($arr_ymd2);
+        $hasil = date_sort($date2, $date1);
+        if($hasil > 0){
+            $datek1 = $date1;
+            $datek2 = $date2;
+        }else{
+            $datek1 = $date2;
+            $datek2 = $date1;
         }
-        else{
-            echo "Tugas atau praktikum? Cek masukkan kamu ya :)";
-        }
+        $sql = "SELECT * FROM tabel WHERE (date BETWEEN '$datek1' AND '$datek2')";
+    }else if(preg_match_all("/[0-9] [Mm]inggu/",$getMesg, $tampung)){
+        //Deadline N minggu ke depan
+        $minggu = sprintf($tampung[0][0][0]);
+        $Nminggu = $minggu + 0;
+        $sql = "SELECT * FROM tabel WHERE date BETWEEN CURDATE() AND CURDATE() + INTERVAL $Nminggu WEEK";
+    }else if(preg_match_all("/[0-9] [Hh]ari/",$getMesg, $tampung)){
+        //Deadline N hari ke depan
+        $hari = sprintf($tampung[0][0][0]);
+        $Nhari = $hari + 0;
+        $sql = "SELECT * FROM tabel WHERE date BETWEEN CURDATE() AND CURDATE() + INTERVAL $Nhari DAY";
+    }else if(preg_match_all("/[Hh]ari [Ii]ni/",$getMesg, $tampung)){
+        //Deadline Hari ini
+        $sql = "SELECT * FROM tabel WHERE date = CURDATE()";
     }else{
-        echo "Maaf, kku gatau kode matkulnya? Cek masukkan kamu ya :)";
+        //Deadline keseluruhan
+        $sql = "SELECT * FROM tabel";
     }
-}
-
-if (preg_match("/[Aa]pa/",$getMesg)){
-    $sql = "SELECT * FROM tabel";
     if($result = mysqli_query($conn, $sql)){
         if(mysqli_num_rows($result) > 0){
-            echo "<table>";
+            echo "[DAFTAR DEADLINE]";
+            echo "<table class='items'>";
                 echo "<tr>";
+                    echo "<th>ID</th>";
                     echo "<th>Tanggal</th>";
                     echo "<th>Matkul</th>";
                     echo "<th>Jenis</th>";
@@ -78,10 +72,11 @@ if (preg_match("/[Aa]pa/",$getMesg)){
                 echo "</tr>";
             while($row = mysqli_fetch_array($result)){
                 echo "<tr>";
-                    $tanggal = DateTime::createFromFormat('Y-m-d', $row['date'])->format('d/m/Y');
-                    echo "<td>" . $tanggal . "</td>";
-                    echo "<td>" . $row['matkul'] . "</td>";
-                    echo "<td>" . $row['katapenting'] . "</td>";
+                    $tanggal = DateTime::createFromFormat('Y-m-d', $row['date'])->format('d-m-Y');
+                    echo "<td>" . $row['id'];
+                    echo "<td style='font-size:xx-small;font-weight:700'>" . $tanggal;
+                    echo "<td>" . $row['matkul'];
+                    echo "<td>" . $row['katapenting'];
                     if ($row['topik']!="NULL"){
                         echo "<td>" . $row['topik'] . "</td>";
                     }else{
@@ -93,10 +88,69 @@ if (preg_match("/[Aa]pa/",$getMesg)){
             // Free result set
             mysqli_free_result($result);
         } else{
-            echo "No records matching your query were found.";
+            echo "Tidak ada deadline yang ditemukan pada rentang waktu tersebut.";
         }
     } else{
-        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+    }
+}
+
+//Rapihin ID
+if (preg_match("/[Rr]eset|[Hh]apus [Ss]emua|[Dd]elete [Aa]ll/",$getMesg)){
+    $sql = "ALTER TABLE tabel DROP id;";
+    $sql .= "ALTER TABLE  `tabel` ADD `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST";
+    if (mysqli_multi_query($conn, $sql)) {
+        echo "Deadline berhasil disetting ulang";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+
+//Insert Deadline baru
+if (preg_match_all("/(0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-[0-9]{4}/",$getMesg,$arr_date) && !preg_match("/[Aa]pa/",$getMesg)) {
+    for ($i = 0; $i < count($arr_date[0]); $i++){
+        $arr_ymd = DateTime::createFromFormat('d-m-Y', $arr_date[0][$i])->format('Y-m-d');
+        $date = sprintf($arr_ymd);
+        echo $date."\n";
+    }
+    if (preg_match_all("/[A-Z]{2}[0-9]{4}/",$getMesg,$ArrMatkul)){
+        $Matkul = sprintf($ArrMatkul[0][0]);
+        echo $Matkul."\n";
+        if (preg_match("/[Tt]ugas [Kk]ecil|[Tt]ucil/",$getMesg)){
+            $KataPenting = "Tucil";
+        }else if (preg_match("/[Tt]ugas [Bb]esar|[Tt]ubes/",$getMesg)){
+            $KataPenting = "Tubes";
+        }else if (preg_match("/[Pp]raktikum|[Pp]rak/",$getMesg)){
+            $KataPenting = "Praktikum";
+        }else if (preg_match("/[Kk]uis/",$getMesg)){
+            $KataPenting = "Kuis";
+        }else if (preg_match("/[Uu]jian [Tt]engah [Ss]emester|[Uu][Tt][Ss]/",$getMesg)){
+            $KataPenting = "UTS";
+        }else if (preg_match("/[Uu]jian [Aa]khir [Ss]emester|[Uu][Aa][Ss]/",$getMesg)){
+            $KataPenting = "UAS";
+        }
+        else{
+            echo " - Tugas atau praktikum? Cek masukkan kamu ya :)";
+            $KataPenting = "NULL";
+            return;
+        }
+        echo $KataPenting."\n";
+        if(preg_match_all("/topik(.*)/", $getMesg,$ArrTopik)){
+            $Topik = sprintf($ArrTopik[1][0]);
+            echo $Topik."\n";
+        }else{
+            $Topik = "NULL";
+        }
+
+        $sql = "INSERT INTO tabel (`date`, `matkul`, `katapenting`, `topik`) VALUES ('$date', '$Matkul', '$KataPenting','$Topik')";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "<br> Deadline berhasil dimasukkan";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    }else{
+        echo "Maaf, aku gatau kode matkulnya? Cek masukkan kamu ya :)";
     }
 }
 
